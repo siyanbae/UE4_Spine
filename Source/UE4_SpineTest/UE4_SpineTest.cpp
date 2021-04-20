8c4d69875c65730ef804b4ddbc4fec8e8cb80ef1
siyanbae <f1f514@gmail.com>
2021-04-20 23:14:07 +0900

    project commit

---
+++ Source/UE4_SpineTest/UE4_SpineTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE4_SpineTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE4_SpineTest, "UE4_SpineTest" );
---
+++ Source/UE4_SpineTest/UE4_SpineTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4_SpineTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4_SPINETEST_API AUE4_SpineTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
